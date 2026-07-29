4229c1041b73fa59be966d1592c001e9273ddfed
Mosa <mosandlovu5@gmail.com>
2026-07-29 13:33:35 +0200

    Added return book function

--- library.py
+++ library.py
@@ -98,8 +98,25 @@ def add_book():
     print(member_id + " borrowed " + book_id)
 
 
-def return_book():
-    print("Return Book - Coming Soon")
+    def return_book(books, members):
+
+    member_id = input("Member ID: ").strip()
+
+    if member_id not in members:
+        print("No such member.")
+        return
+
+    book_id = input("Book ID: ").strip()
+
+    if book_id not in members[member_id]["borrowed"]:
+        print("This member does not have that book.")
+        return
+
+    members[member_id]["borrowed"].remove(book_id)
+    books[book_id]["available"] += 1
+
+    print(member_id + " returned " + book_id)
+
 
 
 def search_books():
@@ -149,7 +166,7 @@ while True:
         borrow_book(books, members)
 
     elif choice == "4":
-        return_book()
+        return_book(books, members)
 
     elif choice == "5":
         search_books()
