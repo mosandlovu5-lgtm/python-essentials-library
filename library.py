2b7ecc7fb289a79cd99f8e1073bcd88383a79eec
Mosa <mosandlovu5@gmail.com>
2026-07-29 13:12:05 +0200

    Added borrow a book function

--- library.py
+++ library.py
@@ -63,8 +63,39 @@ def add_book():
     return next_member_number + 1 
 
 
-def borrow_book():
-    print("Borrow Book - Coming Soon")
+    def borrow_book(books, members):
+
+    member_id = input("Member ID: ").strip()
+
+    if member_id not in members:
+        print("No such member.")
+        return
+
+    book_id = input("Book ID: ").strip()
+
+    if book_id not in books:
+        print("No such book.")
+        return
+
+    if len(members[member_id]["borrowed"]) >= 3:
+        print("Member has reached the borrowing limit.")
+        return
+
+    if book_id in members[member_id]["borrowed"]:
+        print("Member already borrowed this book.")
+        return
+
+    if books[book_id]["available"] == 0:
+        print("No copies available.")
+        return
+
+    # Borrow book
+    books[book_id]["available"] -= 1
+    books[book_id]["times_borrowed"] += 1
+
+    members[member_id]["borrowed"].append(book_id)
+
+    print(member_id + " borrowed " + book_id)
 
 
 def return_book():
@@ -115,7 +146,7 @@ while True:
 ) 
 
     elif choice == "3":
-        borrow_book()
+        borrow_book(books, members)
 
     elif choice == "4":
         return_book()
